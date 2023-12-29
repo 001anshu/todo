8ebcb94abf7856ea84574e0a109b69ba68c39d5b
ANSHU KUMAR SAW <anshusaw7563@gmail.com>
2023-12-29 18:47:15 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-function addtodo(){
+let todoList=['anshu','anss'];
+window.onload=function(){
+   const storedList=localStorage.getItem('todoList');
+   if(storedList){
+      todoList=JSON.parse(localStorage.getItem("todoList"))
+      displayItem();
+   }
+}
+
+function addTodo(){
+   
    let inputElement=document.querySelector(".input")
    let todoItem=inputElement.value;
-   console.log(todoItem);
+   if(todoItem!=''){
+
+      todoList.push(todoItem);
+      inputElement.value='';
+      displayItem();
+      saveToLocalStorage();
+   }
 }
+function deleteTodo(index){
+   todoList.splice(index,1);
+   displayItem();
+   saveToLocalStorage();
+}
+function saveToLocalStorage(){
+   localStorage.setItem('todoList',JSON.stringify(todoList));
+}
+function displayItem() {
+   let containerElement = document.querySelector('.todo-container');
+   let newHtml = '';
+   for (let i = 0; i < todoList.length; i++) {
+       newHtml += `
+       <div class="entry">
+           <span>${todoList[i]}</span>
+           <button class="button-18 input"  onclick="deleteTodo(${i})">delete</button>
+       </div>
+       `;
+   }
+   containerElement.innerHTML = newHtml;
+}
+
